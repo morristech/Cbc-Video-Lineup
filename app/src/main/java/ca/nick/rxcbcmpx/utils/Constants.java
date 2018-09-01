95171c30fb08e4d62e47c9ee35e1f3ad8d7ff2d4
Nick Rose <nicholas.rose@cbc.ca>
2018-09-01 18:10:51 -0400

    Add valid ad url

--- app/src/main/java/ca/nick/rxcbcmpx/utils/Constants.java
+++ app/src/main/java/ca/nick/rxcbcmpx/utils/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
     public static final String USER_AGENT_VALUE = "CBC/AndroidApp";
     public static final String USER_AGENT = USER_AGENT_KEY + ": " + USER_AGENT_VALUE;
 
-    public static final String SAMPLE_AD_URL = "it's a secret";
+    public static final String SAMPLE_AD_URL = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=";
     public static final String LIVE_STATE = "live";
     public static final String SRT_CAPTIONS = "text/srt";
 }
